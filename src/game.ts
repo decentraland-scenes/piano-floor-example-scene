1e1f85b7a086eee1273dab71b9ed3b13bf43edcc
Tak-John Cheung <tak_john@hotmail.com>
2020-05-19 22:19:16 +0100

    updated skipKey condition

--- src/game.ts
+++ src/game.ts
@@ -81,7 +81,7 @@ for (let i = 0; i < blackKeySounds.length; i++) {
   // Skip key
   skipKey++
   if (skipKey == 7) skipKey = 2
-  skipKey != 3 && skipKey != 6 ? (blackKeyXPos += 0.8) : (blackKeyXPos += 1.6)
+  skipKey % 3 != 0 ? (blackKeyXPos += 0.8) : (blackKeyXPos += 1.6)
 }
 
 // Modify player's trigger shape
